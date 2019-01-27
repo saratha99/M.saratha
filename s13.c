7bb75bbaf4e88645be983d1c99ec9e833f14588b
saratha99 <46121206+saratha99@users.noreply.github.com>
2019-01-27 20:00:30 +0530

    Create s13.c

---
+++ s13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'|||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+{
+printf("vowel');]
+else
+{
+printf("constant");
+}
+getch();
+}
+
