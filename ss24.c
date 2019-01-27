7c8b8b0eaa64fc5eba1b24b77c804869398ba071
saratha99 <46121206+saratha99@users.noreply.github.com>
2019-01-28 00:46:52 +0530

    Create ss24.c

---
+++ ss24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+   int fact=1,n,i;
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       fact=fact*i;
+   }
+   printf("%d",fact);
+   getch();
+}
